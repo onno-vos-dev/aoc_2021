860b71b4fe857b5d18cb25b5e686ff2085e5262d
Onno Vos <onno-vos-dev@users.noreply.github.com>
2021-12-03 10:09:10 +0100

    Further speedups of day3

--- apps/aoc/src/util.erl
+++ apps/aoc/src/util.erl
@@ -2,6 +2,7 @@
 
 -export([ read_file/3
         , binary_to_decimal/1
+        , split_to_chunks/2
         , time_avg/2
         ]).
 
@@ -17,17 +18,22 @@ time_avg(Fun, X) ->
                              end, lists:seq(1, X))) / X,
   io:format("Time: ~p us ~.5f ms ~n~n", [AvgTimeMicro, AvgTimeMicro / 1000]).
 
-binary_to_decimal(<<"">>) ->
-  0;
-binary_to_decimal(Binary) when is_binary(Binary) ->
-  binary_to_decimal(binary_to_list(Binary), 0).
+binary_to_decimal(Binary) ->
+  binary_to_integer(Binary, 2).
 
-binary_to_decimal([], Sum) ->
-  Sum;
-binary_to_decimal([48 | Tail], Sum) -> %48 is ascii code for 0
-  binary_to_decimal(Tail, Sum * 2);
-binary_to_decimal([49 | Tail], Sum) -> %49 is ascii code for 1
-  binary_to_decimal(Tail, Sum * 2 + 1).
+split_to_chunks(L, N) when N > 0 ->
+  Len = length(L),
+  case Len =< N of
+    true -> [L];
+    false ->
+      split_to_chunks(L, Len, N, [])
+  end.
+
+split_to_chunks(L, Len, N, Acc) when Len =< N -> lists:reverse([L | Acc]);
+split_to_chunks(L, _, N, Acc) ->
+  {L1, L2} = lists:split(N, L),
+  split_to_chunks(L2, length(L2), N, [L1 | Acc]).
+
 
 %%%_* Emacs ====================================================================
 %%% Local Variables:
