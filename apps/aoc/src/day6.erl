4309091a15af12700ca67d324745a91e4cb834ab
Onno Vos <onno-vos-dev@users.noreply.github.com>
2021-12-06 12:48:48 +0100

    Minor cleanup of day6

--- apps/aoc/src/day6.erl
+++ apps/aoc/src/day6.erl
@@ -1,18 +1,16 @@
 -module(day6).
 
--export([ solve/0, initialize/1, input/0 ]).
+-export([ solve/0, solve/1, input/0 ]).
 
 solve() ->
-  Acc = initialize(input()),
-  Output1 = fishes({0, 80}, Acc),
+  solve(input()).
+
+solve(Input) ->
+  Output1 = fishes({0, 80}, Input),
   Output2 = fishes({80, 256}, Output1),
   Part1 = lists:sum(maps:values(Output1)),
   Part2 = lists:sum(maps:values(Output2)),
-  {Part1, Part2}.
-
-initialize(Input) ->
-  Acc = #{ 0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0},
-  lists:foldl(fun(In, A) -> maps:update(In, maps:get(In, A) + 1, A) end, Acc, Input).
+  {352872, 1604361182149} = {Part1, Part2}.
 
 fishes({Day, MaxDays}, Acc) when Day =:= MaxDays -> Acc;
 fishes({Day, MaxDays}, #{ 0 := Zero
@@ -36,4 +34,10 @@ fishes({Day, MaxDays}, #{ 0 := Zero
   fishes({Day + 1, MaxDays}, NewAcc).
 
 input() ->
-  util:read_file("day6.txt", <<",">>, fun erlang:binary_to_integer/1).
+  Acc = #{ 0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0},
+  FoldFun = fun(B, A) ->
+              Int = binary_to_integer(B),
+              #{Int := Old} = A,
+              A#{Int => Old + 1}
+            end,
+  util:read_file_fold("day6.txt", <<",">>, FoldFun, Acc).
