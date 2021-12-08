07cfeae2363c33b5cad9696636e71ab30ff6dd3f
Onno Vos <onno-vos-dev@users.noreply.github.com>
2021-12-08 09:10:42 +0100

    Have fun and do day7 with some NIFs in Rust

--- apps/aoc/src/day7.erl
+++ apps/aoc/src/day7.erl
@@ -2,25 +2,36 @@
 
 -export([ solve/0, solve/1, input/0 ]).
 
+-dialyzer({nowarn_function, [ solve/0
+                            , solve/1
+                            , input/0
+                            , calc_fuel/3
+                            , do_calc_fuel/4
+                            ]}).
+
 solve() ->
   solve(input()).
 
 solve(Input) ->
-  Part1 = calculate_fuel(floor(util:median(Input)), Input, fun(A) -> A end),
-  Part2 = calculate_fuel(ceil(util:mean(Input)) - 1, Input, fun termial/1),
-  {348996, 98231647} = {Part1, Part2}.
+  Median = floor(util:median_nif(Input)),
+  Mean = ceil(util:mean_nif(Input)) - 1,
+  {348996, 98231647} = calc_fuel({Median, Mean}, Input, fun termial/1).
 
-calculate_fuel(H, Input, F) ->
-  round(do_calculate_fuel(H, Input, F, 0)).
+calc_fuel({Median, Mean}, Input, F) ->
+  do_calc_fuel({Median, Mean}, Input, F, {0, 0}).
 
-do_calculate_fuel(_Pos, [], _F, Acc) -> Acc;
-do_calculate_fuel(Pos, [H | T], F, Acc) ->
-  Move = Pos - H,
-  case Move >= 0 of
-    true ->
-      do_calculate_fuel(Pos, T, F, F(Move) + Acc);
-    false ->
-      do_calculate_fuel(Pos, T, F, F(Move * -1) + Acc)
+do_calc_fuel({_Median, _Mean}, [], _F, {AccMedian, AccMean}) -> {AccMedian, AccMean};
+do_calc_fuel({Median, Mean}, [H | T], F, {AccMedian, AccMean}) ->
+  {MMedian, MMove} = {Median - H, Mean - H},
+  case {MMedian >= 0, MMove >= 0} of
+    {true, true} ->
+      do_calc_fuel({Median, Mean}, T, F, {MMedian + AccMedian, F(MMove) + AccMean});
+    {true, false} ->
+      do_calc_fuel({Median, Mean}, T, F, {MMedian + AccMedian, F(MMove * -1) + AccMean});
+    {false, true} ->
+      do_calc_fuel({Median, Mean}, T, F, {MMedian * - 1 + AccMedian, F(MMove) + AccMean});
+    {false, false} ->
+      do_calc_fuel({Median, Mean}, T, F, {MMedian * - 1 + AccMedian, F(MMove * -1) + AccMean})
   end.
 
 termial(X) -> X * (X + 1) div 2.
--- apps/aoc/src/util.erl
+++ apps/aoc/src/util.erl
@@ -6,10 +6,26 @@
         , time_avg/2
         , time_avg/4
         , median/1
+        , median_nif/1
         , mean/1
+        , mean_nif/1
         , binary_to_decimal/1
         , split_to_chunks/2
+        , sort_ints/1
         ]).
+
+-dialyzer({nowarn_function, [ mean_nif/1
+                            , median_nif/1
+                            , sort_ints/1
+                            , load_mean_nif/1
+                            , load_median_nif/1
+                            , load_sort_ints_nif/1
+                            ]}).
+
+-on_load(init/0).
+
+-define(APPNAME, aoc).
+-define(LIBNAME, "libaoc").
 
 %% API ========================================================================
 -spec read_file(string(), binary()) -> [any()].
@@ -56,6 +72,23 @@ median(Unsorted) ->
 
 mean(L) -> lists:sum(L) / length(L).
 
+%%%_* API - NIFS ==============================================================
+sort_ints(A) -> load_sort_ints_nif(A).
+
+mean_nif(A) -> load_mean_nif(A).
+
+median_nif(A) -> load_median_nif(A).
+
+%%%_* NIFS ====================================================================
+load_sort_ints_nif(_) ->
+  not_loaded(?LINE).
+
+load_mean_nif(_) ->
+  not_loaded(?LINE).
+
+load_median_nif(_) ->
+  not_loaded(?LINE).
+
 %%%_* Internal ================================================================
 -spec read(string()) -> {error, atom()} | {ok, binary()}.
 read(File) ->
@@ -77,3 +110,20 @@ split_to_chunks(N, N, L, Acc) ->
   [Acc | split_to_chunks(N, 0, L, [])];
 split_to_chunks(N, X, [H|T], Acc) ->
   split_to_chunks(N, X + 1, T, [H|Acc]).
+
+init() ->
+  SoName = case code:priv_dir(?APPNAME) of
+              {error, bad_name} ->
+                case filelib:is_dir(filename:join(["..", priv])) of
+                  true ->
+                    filename:join(["..", priv, ?LIBNAME]);
+                  _ ->
+                    filename:join([priv, ?LIBNAME])
+                end;
+              Dir ->
+                filename:join(Dir, ?LIBNAME)
+            end,
+  erlang:load_nif(SoName, 0).
+
+not_loaded(Line) ->
+  exit({not_loaded, [{module, ?MODULE}, {line, Line}]}).
