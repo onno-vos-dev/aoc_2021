2204a7d461b3c702ffcc4eb4c82bd0948fb07045
Onno Vos <onno-vos-dev@users.noreply.github.com>
2021-12-10 00:37:52 +0100

    Optimize day7 by only looping through input once

--- rust/src/day7.rs
+++ rust/src/day7.rs
@@ -15,33 +15,29 @@ fn mean(arr: Vec<i32>, len: usize) -> i32 {
   arr.iter().sum::<i32>() as i32 / len as i32
 }
 
-fn calc_fuel_part1(median: i32, input: Vec<i32>) -> i32 {
-  input.iter()
-    .fold(0, |acc: i32, i| {
-      if median - i >= 0 {
-        acc + (median - i)
-      } else {
-        acc + ((median - i) * -1)
-      }
-    })
-}
-
-fn calc_fuel_part2(mean: i32, input: Vec<i32>) -> i32 {
-  input.iter()
-    .fold(0, |acc: i32, i| {
-      if mean - i >= 0 {
-        acc + termial(mean - i)
-      } else {
-        acc + termial((mean - i) * -1)
-      }
-    })
+fn calc_fuel(median: i32, mean: i32, input: Vec<i32>) -> (i32, i32) {
+  let mut median_acc: i32 = 0;
+  let mut mean_acc: i32 = 0;
+  for i in input.iter() {
+    if median - i >= 0 {
+      median_acc += median - i
+    } else {
+      median_acc += (median - i) * -1
+    }
+    if mean - i >= 0 {
+      mean_acc += termial(mean - i)
+    } else {
+      mean_acc += termial((mean - i) * -1)
+    }
+  }
+  (median_acc, mean_acc)
 }
 
 fn termial(x: i32) -> i32 {
   x * (x + 1) / 2 as i32
 }
 
-fn main() {
+fn day7() -> (u128, i32, i32) {
   let start = Instant::now();
   let mut file = File::open("../../aoc_2021/apps/aoc/priv/inputs/day7.txt").expect("file wasn't found.");
   let mut s = String::new();
@@ -49,16 +45,24 @@ fn main() {
   let numbers_str: Vec<&str> = s.split(",").collect();
   let numbers: Vec<i32> = numbers_str.iter().map(|s| s.parse::<i32>().unwrap()).collect();
   let len: usize = numbers.len();
-  let input1: Vec<i32> = numbers.clone();
-  let input2: Vec<i32> = numbers.clone();
+  let input: Vec<i32> = numbers.clone();
   let l1: Vec<i32> = numbers.clone();
   let l2: Vec<i32> = numbers.clone();
   let median = median(l1, len);
   let mean = mean(l2, len);
-  let part1 = calc_fuel_part1(median, input1);
-  let part2 = calc_fuel_part2(mean, input2);
-  let end = start.elapsed();
-  println!("Took: {:?}", end);
+  let (part1, part2) = calc_fuel(median, mean, input);
+  let end = start.elapsed().as_nanos();
+  (end, part1, part2)
+}
+
+fn main() {
+  let mut average: u128 = 0;
+  for _ in 1..1000 {
+    let (time, _part1, _part2) = day7();
+    average += time
+  }
+  let (_time, part1, part2) = day7();
+  println!("Average time after 1000 iterations: {} nanoseconds ({} us) ", average / 1000, average / 1000000);
   println!("Part1: {}", part1);
   println!("Part2: {}", part2)
 }
